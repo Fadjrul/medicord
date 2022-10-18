93c086a0f01a20c0f6f53bc3d8fd37c72f5017ea
iqbaltahir33 <iqbalknightphantom33@gmail.com>
2022-10-18 19:21:58 +0800

    add change

--- application/controllers/Pasien.php
+++ application/controllers/Pasien.php
@@ -1,81 +1,86 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
-class Pasien extends CI_Controller {
-    public function __construct() {
+defined('BASEPATH') or exit('No direct script access allowed');
+class Pasien extends CI_Controller
+{
+    public function __construct()
+    {
         parent::__construct();
         $this->load->model('m_pasien');
-        if (!$this->session->userdata('id_user') OR $this->session->userdata('user_group')!=1) {
-			// ALERT
-			$alertStatus  = 'failed';
-			$alertMessage = 'Anda tidak memiliki Hak Akses atau Session anda sudah habis';
-			getAlert($alertStatus, $alertMessage);
-			redirect('auth');
-		}
+        if (!$this->session->userdata('id_user') or $this->session->userdata('user_group') != 1) {
+            // ALERT
+            $alertStatus  = 'failed';
+            $alertMessage = 'Anda tidak memiliki Hak Akses atau Session anda sudah habis';
+            getAlert($alertStatus, $alertMessage);
+            redirect('auth');
+        }
     }
-    
 
-    public function index() {
+
+    public function index()
+    {
         $this->session->unset_userdata('sess_search_pasien');
 
         // PAGINATION
         $baseUrl    = base_url() . "pasien/index/";
-        $totalRows  = count((array) $this->m_pasien->read('','',''));
+        $totalRows  = count((array) $this->m_pasien->read('', '', ''));
         $perPage    = $this->session->userdata('sess_rowpage');
         $uriSegment = 4;
         $paging     = generatePagination($baseUrl, $totalRows, $perPage, $uriSegment);
         $page       = ($this->uri->segment($uriSegment)) ? $this->uri->segment($uriSegment) : 0;
-        
+
         $data['numbers']    = $paging['numbers'];
         $data['links']      = $paging['links'];
-        $data['total_data'] = $totalRows ;
-        
+        $data['total_data'] = $totalRows;
 
-        
+
+
         //DATA
         $data['setting']       = getSetting();
         $data['title']         = 'Data Pasien';
-        $data['pasien']        = $this->m_pasien->read($perPage, $page,'');
-		
-        
-        // TEMPLATE
-		$view         = "pasien/index";
-		$viewCategory = "all";
-		TemplateApp($data, $view, $viewCategory);
-    }
-    
+        $data['pasien']        = $this->m_pasien->read($perPage, $page, '');
 
-    public function search() {
+
+        // TEMPLATE
+        $view         = "pasien/index";
+        $viewCategory = "all";
+        TemplateApp($data, $view, $viewCategory);
+    }
+
+
+    public function search()
+    {
         if ($this->input->post('key')) {
             $data['search'] = $this->input->post('key');
             $this->session->set_userdata('sess_search_pasien', $data['search']);
         } else {
             $data['search'] = $this->session->userdata('sess_search_pasien');
         }
-        
+
         // PAGINATION
-        $baseUrl    = base_url() . "pasien/search/".$data['search']."/";
-        $totalRows  = count((array)$this->m_pasien->read('','',$data['search']));
+        $baseUrl    = base_url() . "pasien/search/" . $data['search'] . "/";
+        $totalRows  = count((array)$this->m_pasien->read('', '', $data['search']));
         $perPage    = $this->session->userdata('sess_rowpage');
         $uriSegment = 5;
         $paging     = generatePagination($baseUrl, $totalRows, $perPage, $uriSegment);
         $page       = ($this->uri->segment($uriSegment)) ? $this->uri->segment($uriSegment) : 0;
-        
+
         $data['numbers']    = $paging['numbers'];
         $data['links']      = $paging['links'];
-        $data['total_data'] = $totalRows ;
-        
+        $data['total_data'] = $totalRows;
+
         //DATA
         $data['setting']       = getSetting();
         $data['title']         = 'Data Pasien';
         $data['pasien']        = $this->m_pasien->read($perPage, $page, $data['search']);
-        
+
         // TEMPLATE
-		$view         = "pasien/index";
-		$viewCategory = "all";
-		TemplateApp($data, $view, $viewCategory);
+        $view         = "pasien/index";
+        $viewCategory = "all";
+        TemplateApp($data, $view, $viewCategory);
     }
-    
-    public function create_page() {
+
+    public function create_page()
+    {
         // PAGE
         $uriSegment = 3;
         $page       = ($this->uri->segment($uriSegment)) ? $this->uri->segment($uriSegment) : 0;
@@ -83,32 +88,39 @@ class Pasien extends CI_Controller {
         //DATA
         $data['setting']       = getSetting();
         $data['title']         = 'Tambah Data Pasien';
-        $data['pasien'] = $this->m_pasien->read('','','');
-		
+        $data['pasien'] = $this->m_pasien->read('', '', '');
+
         // TEMPLATE
-		$view         = "pasien/add";
-		$viewCategory = "all";
-		TemplateApp($data, $view, $viewCategory);
+        $view         = "pasien/add";
+        $viewCategory = "all";
+        TemplateApp($data, $view, $viewCategory);
     }
 
-    public function create() {
+    public function create()
+    {
         csrfValidate();
+
         // POST
-        $data['id_pasien']   = '';
+        $data['id_pasien']   = '000' . $this->input->post('id_user');
         $data['nama_pasien'] = $this->input->post('nama_pasien');
+        $data['nik_pasien'] = $this->input->post('nik_pasien');
+        $data['nama_kepala_keluarga'] = $this->input->post('nama_kepala_keluarga');
+        $data['jenis_kelamin'] = $this->input->post('jenis_kelamin');
+        $data['tgl_lahir_pasien'] = $this->input->post('jenis_kelamin');
         $data['createtime']  = date('Y-m-d H:i:s');
         $this->m_pasien->create($data);
 
         // ALERT
         $alertStatus  = "success";
-        $alertMessage = "Berhasil menambah data pasien ".$data['nama_pasien'];
+        $alertMessage = "Berhasil menambah data pasien " . $data['nama_pasien'];
         getAlert($alertStatus, $alertMessage);
 
         redirect('pasien/index');
     }
-    
 
-    public function update() {
+
+    public function update()
+    {
         csrfValidate();
         // POST
         $data['id_pasien']   = $this->input->post('id_pasien');
@@ -117,25 +129,24 @@ class Pasien extends CI_Controller {
 
         // ALERT
         $alertStatus  = "success";
-        $alertMessage = "Berhasil mengubah data pasien : ".$data['nama_pasien'];
+        $alertMessage = "Berhasil mengubah data pasien : " . $data['nama_pasien'];
         getAlert($alertStatus, $alertMessage);
 
         redirect('pasien/index');
     }
-    
 
-    public function delete() {
+
+    public function delete()
+    {
         csrfValidate();
         // POST
         $this->m_pasien->delete($this->input->post('id_pasien'));
 
         // ALERT
         $alertStatus  = "failed";
-        $alertMessage = "Menghapus data pasien : ".$this->input->post('nama_pasien');
+        $alertMessage = "Menghapus data pasien : " . $this->input->post('nama_pasien');
         getAlert($alertStatus, $alertMessage);
 
-        redirect('pasien/index');
+        redirect('pasien/');
     }
-    
 }
-?>
